e34ddc65bb809b94a1a2a055abca707e8bef6464
shari089 <sybtano22@src.pshs.edu.ph>
2025-02-25 19:49:23 +0800

    Update wordle.js

--- wordle.js
+++ wordle.js
@@ -22,13 +22,14 @@ function checkWord() {
         alert("Invalid input. Please enter a 5-letter word.");
         return;
     }
-
-    let box0 = document.getElementById(r-${5-health}-0);
-    let box1 = document.getElementById(r-${5-health}-1);
-    let box2 = document.getElementById(r-${5-health}-2);
-    let box3 = document.getElementById(r-${5-health}-3);
-    let box4 = document.getElementById(r-${5-health}-4);
-
+    
+    let rowIndex = 5 - health;
+    let box0 = document.getElementById(`r-${rowIndex}-0`);
+    let box1 = document.getElementById(`r-${rowIndex}-1`);
+    let box2 = document.getElementById(`r-${rowIndex}-2`);
+    let box3 = document.getElementById(`r-${rowIndex}-3`);
+    let box4 = document.getElementById(`r-${rowIndex}-4`);
+    
     box0.innerText = answer[0];
     box1.innerText = answer[1];
     box2.innerText = answer[2];
@@ -37,23 +38,23 @@ function checkWord() {
 
     // Row 1
     if (answer[0] == chosenWord[0]) {
-        box0.style.backgroundColor = #6AAA64;
+        box0.style.backgroundColor = "#6AAA64";
     } 
     else if (answer[0] == chosenWord[1] || answer[0] == chosenWord[2] || 
         answer[0] == chosenWord[3] || answer[0] == chosenWord[4] ) {
-        box0.style.backgroundColor = #C9B458;
+        box0.style.backgroundColor = "#C9B458";
     } 
     else {
-        box0.style.backgroundColor = #787C7E;
+        box0.style.backgroundColor = "#787C7E";
     }
 
     // Row 2
     if (answer[1] == chosenWord[1]) {
-        box1.style.backgroundColor = #6AAA64;
+        box1.style.backgroundColor = "#6AAA64";
     } 
     else if (answer[1] == chosenWord[0] || answer[1] == chosenWord[2] || 
         answer[1] == chosenWord[3] || answer[1] == chosenWord[4]) {
-        box1.style.backgroundColor = #C9B458;
+        box1.style.backgroundColor = "#C9B458";
     } 
     else {
         box1.style.backgroundColor = #787C7E;
@@ -61,36 +62,36 @@ function checkWord() {
 
     // Row 3
     if (answer[2] == chosenWord[2]) {
-        box2.style.backgroundColor = #6AAA64;
+        box2.style.backgroundColor = "#6AAA64";
     } else if (answer[2] == chosenWord[0] || answer[2] == chosenWord[1] || 
         answer[2] == chosenWord[3] || answer[2] == chosenWord[4]) {
-        box2.style.backgroundColor = #C9B458;
+        box2.style.backgroundColor = "#C9B458";
     }
     else {
-        box2.style.backgroundColor = #787C7E;
+        box2.style.backgroundColor = "#787C7E";
     }
 
     // Row 4
     if (answer[3] == chosenWord[3]) {
-        box3.style.backgroundColor = #6AAA64;
+        box3.style.backgroundColor = "#6AAA64";
     } 
     else if (answer[3] == chosenWord[0] || answer[3] == chosenWord[1] || 
         answer[3] == chosenWord[2] || answer[3] == chosenWord[4]) {
-        box3.style.backgroundColor = #C9B458;
+        box3.style.backgroundColor = "#C9B458";
     } else {
-        box3.style.backgroundColor = #787C7E;
+        box3.style.backgroundColor = "#787C7E";
     }
 
     // Row 5
     if (answer[4] == chosenWord[4]) {
-        box4.style.backgroundColor = #6AAA64;
+        box4.style.backgroundColor = "#6AAA64";
     } 
     else if (answer[4] == chosenWord[0] || answer[4] == chosenWord[1] || 
         answer[4] == chosenWord[2] || answer[4] == chosenWord[3]) {
-        box4.style.backgroundColor = #C9B458;
+        box4.style.backgroundColor = "#C9B458";
     } 
     else {
-        box4.style.backgroundColor = #787C7E;
+        box4.style.backgroundColor = "#787C7E";
     }
 
     health--;
@@ -101,6 +102,6 @@ function checkWord() {
     }
 
     if (health == 0) {
-        alert(Nice try! The word was ${chosenWord}.\n\nReload the page to try again!);
+        alert(`Nice try! The word was ${chosenWord}.\n\nReload the page to try again!`);
     }
 }
