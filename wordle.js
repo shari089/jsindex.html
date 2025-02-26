aee07bbfc6da5775c5da90a371ed57a295032f20
shari089 <sybtano22@src.pshs.edu.ph>
2025-02-26 13:26:21 +0800

    Update wordle.js

--- wordle.js
+++ wordle.js
@@ -16,7 +16,7 @@ function setupGame() {
 function checkWord() {
     if (health <= 0) return;
     
-    let rowIndex = 5 - health; 
+    let rowIndex = 5 - health - 1; 
 
     let answer = prompt("Enter a word: ").toUpperCase();
 
